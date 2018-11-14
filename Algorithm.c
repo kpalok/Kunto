64c44b1953ad290680c4947c9a340459456808cc
Joona Halkola <joona.halkola@hotmail.com>
2018-11-14 13:37:54 +0200

    algoritmi versio miljoonayks

--- Algorithm.c
+++ Algorithm.c
@@ -12,10 +12,10 @@ float CalcMeanFloat(float *data){
 	//Calculate mean for float values
 	int i;
 	float sum = 0;
-	for (i = 0; i < 10; i++){
+	for (i = 0; i < 20; i++){
 		sum += data[i];
 	}
-	return sum / 10;
+	return sum / 20;
 }
 
 
@@ -23,10 +23,10 @@ double CalcMeanDouble(double data[]){
 	//Calculate mean for double values
 	int i;
 	double sum = 0;
-	for (i = 0; i < 10; i++){
+	for (i = 0; i < 20; i++){
 		sum += data[i];
 	}
-	return sum / 10;
+	return sum / 20;
 }
 
 
@@ -34,10 +34,10 @@ float CalcVar(float data[], float avg){
 	//Calculate variance
 	int i;
 	float sum = 0;
-	for (i = 0; i < 10; i++){
+	for (i = 0; i < 20; i++){
 		sum += (data[i] - avg)*(data[i] - avg);
 	}
-	return sum / 9;
+	return sum / 19;
 }
 
 
@@ -60,7 +60,7 @@ MovementState CalcState(float ax[], float ay[], float az[], double pres[], doubl
 	//Check which state user is in
 
 	//Lift Up
-	if (0.00016 <= zVar && zVar <= 0.0043 && difToLastPres < -0.06 && difToLastPres)
+	if (0.0006 <= zVar && zVar <= 0.0043 && difToLastPres < -0.08 && difToLastPres)
 	{
 		return LiftUp;
 	}
@@ -70,12 +70,12 @@ MovementState CalcState(float ax[], float ay[], float az[], double pres[], doubl
 		return LiftDown;
 	}
 	//Stairs Up
-	else if (((-0.3 < yMean && yMean < -0.4) || (-0.4 < xMean && xMean < 0.4)) && 0.065 < zVar && zMean < -0.58 && difToLastPres < -0.016)
+	else if (0.07 < xMean && xMean < 0.4 && 0.07 < zVar && zMean < -0.58 && difToLastPres < -0.03)
 	{
 		return StairsUp;
 	}
 	//Stairs Down
-	else if (((-0.3 < yMean && yMean < 0.4) || (0.04 < xMean && xMean < 0.4)) && 0.065 < zVar && zMean < -0.58 && difToLastPres > 0.016)
+	else if (((0.04 < yMean && yMean < 0.3) || (0.045 < xMean && xMean < 0.25)) && 0.07 < zVar && zMean < -0.58 && difToLastPres > 0.032)
 	{
 		return StairsDown;
 	}
